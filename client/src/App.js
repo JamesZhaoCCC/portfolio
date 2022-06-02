7e0adb8d7825b2cd6ae5ca946141c70e99caf9fa
JamesZhaoCCC <zhaojames280@gmail.com>
2022-06-02 16:12:01 -0400

    mern practice memories app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import frog3 from './images/frog3.jpg';
+import useStyles from './styles';
 
 const App = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [currentId, dispatch]);
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            {/* 
+                appBar flex does not work - 
+                REASON: Flex direction is being overwritten by .css-1cjrmu8-MuiPaper-root-MuiAppBar-root
+                SOLUTION: force flexDirection using important in styles
+            */}
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+                <img className={classes.image} src={frog3} alt='frog3' height='50'/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    {/* 
+                        mainContainer flex does not work - 
+                        REASON: Flex direction is being overwritten by .css-1vp1qdv-MuiGrid-root
+                        SOLUTION: force flexDirection using important in styles
+                    */}
+                    <Grid className={classes.mainContainer} container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts setCurrentId={setCurrentId} />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+            
+        </Container>
     );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
 
+import './index.css';
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@mui/styles';
+
+const preloadedState = window.__PRELOADED_STATE__;
+
+const store = configureStore({
+    middleware: [thunk],
+    reducer: reducers,
+    preloadedState,
+});
+
+const container = document.getElementById('root');
+
+const root = ReactDOMClient.createRoot(container);
+
+const theme = createTheme();
+
+root.render(
+    <ThemeProvider theme={theme}>
+        <Provider store = {store} serverState = {preloadedState}>
+            <App />
+        </Provider>
+    </ThemeProvider>
+);
 
